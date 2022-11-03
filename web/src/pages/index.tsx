52e7d2dc61f796681b751ad2ad249a7f50cb2b67
Tiago Costa <tiagodpv@gmail.com>
2022-11-02 22:20:23 -0400

    final version

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,6 +6,7 @@ import logoImg from '../assets/logo.svg'
 import usersAvatarExample from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
 interface HomeProps {
   poolCount: number;
@@ -14,6 +15,23 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+  
+
+  async function createPool(event:FormEvent){
+    event.preventDefault();
+
+    try{
+      await api.post('/pools', {
+        title: poolTitle,
+      });
+    }catch(err){
+      console.log(err);
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+
+  }
+
   return (
   <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
     <main>
@@ -30,11 +48,12 @@ export default function Home(props: HomeProps) {
         </strong>
       </div>
 
-      <form onSubmit={} className="mt-10 flex gap-2" >
+      <form onSubmit={createPool} className="mt-10 flex gap-2" >
         <input 
-          className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm"
+          className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm text-gray-100"
           type="text" 
           required placeholder="Qual nome do seu bolão?"
+          onChange={event=>setPoolTitle(event.target.value)}
         />
         <button
           className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700" 
