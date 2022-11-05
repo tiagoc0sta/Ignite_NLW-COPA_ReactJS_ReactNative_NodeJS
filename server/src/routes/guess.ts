8a8b0f08364bc919ad545e0d254f17e0aaeb6748
Tiago Costa <tiagodpv@gmail.com>
2022-11-05 01:12:07 -0400

    route guess

--- server/src/routes/guess.ts
+++ server/src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function guessRoutes(fastify: FastifyInstance){
   fastify.get('/guesses/count', async () => {
@@ -7,4 +9,30 @@ export async function guessRoutes(fastify: FastifyInstance){
 
     return { count }
   })
+
+  fastify.post('/pools/:poolId/games/:gameId/guesses', {
+    onRequest: [authenticate]
+  }, async(request, reply) => {
+    const createGuessParams = z.object({
+      poolId: z.string(), 
+      gameId: z.string(),
+    })
+
+    const createGuessBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+
+    })
+
+    const { poolId, gameId } = createGuessParams.parse(request.params)
+    const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(request.body)
+
+    return {
+      poolId,
+      gameId,
+      firstTeamPoints,
+      secondTeamPoints
+    }
+  })
+
 }
